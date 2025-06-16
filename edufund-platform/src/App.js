10eb7080268261e8fe90a8fad69cc7c422301263
Clarita254 <clara.waithaka@strathmore.edu>
2025-06-16 23:36:56 +0300

    Applied changes to app.jsfile

--- edufund-platform/src/App.js
+++ edufund-platform/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Topnav from "./components/topnav";
-import SignUp from "./pages/SignupPage";
 import SignIn from "./pages/SignInPage";
-
+import SignUp from "./pages/SignupPage";
+import {Navigate} from "react-router-dom";
 
 function App(){
 
@@ -11,6 +11,8 @@ return(
 <Router>
   <Topnav />
   <Routes>
+
+    <Route path="/" element={<Navigate to="/signin" />} />
     <Route path="/signup" element={<SignUp />} />
     <Route path="/signin" element={<SignIn />} />
   </Routes>
