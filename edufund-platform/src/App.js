1aa9799e946e75a5a0e1cfb15a1e313a97fe8c99
Clarita254 <clara.waithaka@strathmore.edu>
2025-06-16 23:42:29 +0300

    Removed a line of code from app.js file

--- edufund-platform/src/App.js
+++ edufund-platform/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Topnav from "./components/topnav";
 import SignIn from "./pages/SignInPage";
-import SignUp from "./pages/SignupPage";
+
 import {Navigate} from "react-router-dom";
 
 function App(){
@@ -13,7 +13,7 @@ return(
   <Routes>
 
     <Route path="/" element={<Navigate to="/signin" />} />
-    <Route path="/signup" element={<SignUp />} />
+    
     <Route path="/signin" element={<SignIn />} />
   </Routes>
 </Router>
